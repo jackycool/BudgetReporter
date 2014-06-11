f1db39a3e47cc0e988ba97bd56b80f34a2f78635
jacky <jacky.tn@gmail.com>
2014-06-11 12:47:14 -0400

    create constructor for the main class

--- src/com/budgetreporter/BudgetReporter.java
+++ src/com/budgetreporter/BudgetReporter.java
@@ -12,18 +12,17 @@ import com.budgetreporter.DB.DBOperations;
  *
  */
 public class BudgetReporter {
+	
+	public BudgetReporter(){}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hi");
 		run();
 	}
 	
 	public static void run(){
-		DBOperations op = new DBOperations();
+		BudgetReporter br = new BudgetReporter();
+		//DBOperations op = new DBOperations();
 		//op.connectToAndQueryDatabaseTest();
 		//op.createTableUsers();
 		//op.CreateTableIncome();
