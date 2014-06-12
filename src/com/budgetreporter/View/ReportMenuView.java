6075042798b8c803128a3bb3de8fc9421f4dff50
jacky <jacky.tn@gmail.com>
2014-06-12 12:55:08 -0400

    Added basic GUI components for reportMenuView

--- src/com/budgetreporter/View/ReportMenuView.java
+++ src/com/budgetreporter/View/ReportMenuView.java
@@ -3,10 +3,40 @@
  */
 package com.budgetreporter.View;
 
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 /**
  * @author jacky
  *
  */
-public class ReportMenuView {
+public class ReportMenuView extends JPanel{
+	
+	public ReportMenuView(){
+		super();
+		initUI();
+	}
+	
+	public void initUI(){
+		GridLayout gLayout = new GridLayout(0,1);
+		this.setLayout(gLayout);
+        
+        //Add the "View Monthly Report" button
+        JButton viewMonthlyReportButton = new JButton("View Monthly Report");
+
+        //Add the "New Annual Report" button
+        JButton viewAnnualReportButton = new JButton("View Annual Report");
+        
+        //Add the "Back" button
+        JButton backButton = new JButton("Back");
+
+        this.add(viewMonthlyReportButton);
+        this.add(viewAnnualReportButton);
+        this.add(backButton);
+        
+        System.out.println("Report Menu page created");		
+	}
 
 }
